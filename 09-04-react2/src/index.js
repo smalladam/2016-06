8b48bb480dcf56fef34f469e41b97db2fb08bcbf
smalladam <13014192356@sina.cn>
2016-09-04 10:00:44 +0800

    third

---
+++ 09-04-react2/src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// import { firstName } from "./components.js";
+// console.log(firstName);
+ import firstName2  from "./components.js";
+ console.log(firstName2);
---
+++ 09-04-react2/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+//console.log(webpack); //可以看到都安了什么有很多东西
+module.exports = {
+    entry: [
+        'webpack/hot/dev-server',
+        path.resolve(__dirname, 'src/index.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        publicPath: "/static/",
+        stats: {
+            colors: true,
+            hot: true
+        },
+        port: 3000,
+        inline: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: 'cheap-module-source-map', //  相当于 webpack -d 让出错的地方可以找到
+    resolve: {
+        extensions: ["", ".js", ".jsx"], //不用写后缀，他会自己依次匹配
+    },
+    module: {   //编译es6
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    }
+
+}
