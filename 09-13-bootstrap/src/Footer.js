48fe219ddfc9e11143de24f9130854c4c8f297c4
smalladam <13014192356@sina.cn>
2016-09-14 15:42:37 +0800

    4

--- 09-13-bootstrap/src/Footer.js
+++ 09-13-bootstrap/src/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render () {
    return(
      <div>
-        <h1 className='text-center'>我是页面的Footer</h1>
+        <h1 className='text-center'>我是页面的Footer部分</h1>
      </div>
    )
   }
